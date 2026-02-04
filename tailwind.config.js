0b1230a1dc3268e48863ba5fce5455ec884e8823
RayySixx <rayyganteng2010@gmail.com>
2026-02-04 21:45:49 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#09090b', // Zinc 950
+        surface: '#18181b',    // Zinc 900
+        primary: '#3b82f6',    // Blue 500
+      },
+    },
+  },
+  plugins: [],
+}
